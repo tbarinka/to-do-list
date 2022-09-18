cbb5eb32df14c40d9b3540a6640a8b1046c5ba38
Taylor Barinka <barinkataylor@gmail.com>
2022-09-18 14:13:56 -0500

    backup before working on listCollapse()

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,34 +3,36 @@ import chevron from './images/chevron-up.png';
 import plus from './images/plus.png';
 import pencil from './images/pencil-outline.png';
 
-// create the Dom elements so as to include:
-    //+create a task
-    //Calendar
-    //Lists
 
 function sidebarDom() {
     const content = document.getElementById('Content')
     const sidebar = document.createElement('div');
         sidebar.classList.add('Sidebar');
     content.appendChild(sidebar);
-    sidebar.appendChild(sidebarItemIntegrater('Lists', 'y'));
+    sidebar.appendChild(createTaskButton());
+    sidebar.appendChild(sidebarHeaderIntegrater('Calendar', 'y'))
+    sidebar.appendChild(sidebarHeaderIntegrater('Lists', 'y'));
 }
-
-//a drop down module with the following elements:
-    //header
-    //drop-down functionality
-    //sub-headings
-function sidebarItemIntegrater(title, toggleRightComponent) {
+function createTaskButton() {
+    const div = document.createElement('div');
+        div.classList.add('SidebarAddTaskButton')
+    const button = document.createElement('button');
+        button.classList.add('createTask');
+        button.textContent = "+ Create a Task"
+    div.appendChild(button);
+    return div;
+}
+function sidebarHeaderIntegrater(title, toggleRightComponent) {
     const div = document.createElement('div');
     div.classList.add('SidebarIntegratedItem');
     
     if (toggleRightComponent == "y") {
         div.appendChild(leftSidebarComponentListCollapse(title))
         div.appendChild(rightSidebarItem());
-    } else {
-        div.appendChild(leftSidebarComponentListCollapse(title))
+    } else if (toggleRightComponent == "n") {
+        div.appendChild(leftSidebarComponent(title))
         const item = document.createElement('div');
-            item.classList.add('SideBarItemRightComponent');
+            item.classList.add('SideBarItemRightComponent-empty');
         div.appendChild(item);
     }
     return div;
