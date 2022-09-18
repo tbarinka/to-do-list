0bf3a50e6ee812b1be53a9482135029607f5b611
Taylor Barinka <barinkataylor@gmail.com>
2022-09-18 11:43:36 -0500

    create header() in sidebar.js

--- src/sidebar.js
+++ src/sidebar.js
@@ -9,10 +9,23 @@ function sidebarDom() {
     const content = document.getElementById('Content')
     const sidebar = document.createElement('div');
         sidebar.classList.add('Sidebar');
-        sidebar.textContent = "awefawefawefa"
-
     content.appendChild(sidebar);
 
-
+    sidebar.appendChild(header('Calendar'));
 }
+
+//a drop down module with the following elements:
+    //header
+    //drop-down functionality
+    //sub-headings
+
+function header(title) {
+    const sideHeader = document.createElement('h3')
+    sideHeader.classList.add('sideBarHeader');
+    sideHeader.textContent = title;
+
+    return sideHeader;
+}
+
+
 export { sidebarDom }
