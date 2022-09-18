3215cef7a55039e233c9a7fc191081b2b6f8ea9a
Taylor Barinka <barinkataylor@gmail.com>
2022-09-18 12:14:45 -0500

    add sidebar header factory functions

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,4 +1,5 @@
 import './style.css';
+import chevron from './images/chevron-up.png';
 
 // create the Dom elements so as to include:
     //+create a task
@@ -11,21 +12,42 @@ function sidebarDom() {
         sidebar.classList.add('Sidebar');
     content.appendChild(sidebar);
 
-    sidebar.appendChild(header('Calendar'));
+    sidebar.appendChild(headerItem('Calendar'));
+    sidebar.appendChild(headerItemListCollapse('Lists'));
 }
 
 //a drop down module with the following elements:
     //header
     //drop-down functionality
     //sub-headings
+function headerItem(title) {
+    const item = document.createElement('div');
+        item.classList.add('SideBarHeaderItem');
 
+    item.appendChild(header(title));
+    return item;
+};
+function headerItemListCollapse(title) {
+    const item = document.createElement('div');
+        item.classList.add('SideBarHeaderItem');
+    item.appendChild(header(title));
+    item.appendChild(listCollapse());
+    return item
+};
 function header(title) {
     const sideHeader = document.createElement('h3')
     sideHeader.classList.add('sideBarHeader');
     sideHeader.textContent = title;
-
     return sideHeader;
-}
+};
+function listCollapse() {
+    const button = document.createElement('button');
+        button.classList.add('sideBarButton');
+    const collapseIcon = new Image();
+    collapseIcon.src = chevron;
+    button.appendChild(collapseIcon);
+    return button;
+};
 
 
 export { sidebarDom }
