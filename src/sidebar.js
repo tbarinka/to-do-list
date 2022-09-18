92599868e05834880541b56254ddcc631150d0a0
Taylor Barinka <barinkataylor@gmail.com>
2022-09-18 13:21:53 -0500

    add sidebarItemIntegrator() to sidebar.js

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,5 +1,7 @@
 import './style.css';
 import chevron from './images/chevron-up.png';
+import plus from './images/plus.png';
+import pencil from './images/pencil-outline.png';
 
 // create the Dom elements so as to include:
     //+create a task
@@ -11,43 +13,81 @@ function sidebarDom() {
     const sidebar = document.createElement('div');
         sidebar.classList.add('Sidebar');
     content.appendChild(sidebar);
-
-    sidebar.appendChild(headerItem('Calendar'));
-    sidebar.appendChild(headerItemListCollapse('Lists'));
+    sidebar.appendChild(sidebarItemIntegrater('Lists', 'y'));
 }
 
 //a drop down module with the following elements:
     //header
     //drop-down functionality
     //sub-headings
-function headerItem(title) {
+function sidebarItemIntegrater(title, toggleRightComponent) {
+    const div = document.createElement('div');
+    div.classList.add('SidebarIntegratedItem');
+    
+    if (toggleRightComponent == "y") {
+        div.appendChild(leftSidebarComponentListCollapse(title))
+        div.appendChild(rightSidebarItem());
+    } else {
+        div.appendChild(leftSidebarComponentListCollapse(title))
+        const item = document.createElement('div');
+            item.classList.add('SideBarItemRightComponent');
+        div.appendChild(item);
+    }
+    return div;
+
+}
+function rightSidebarItem() {
     const item = document.createElement('div');
-        item.classList.add('SideBarHeaderItem');
+        item.classList.add('SideBarItemRightComponent');
+    item.appendChild(editButton());
+    item.appendChild(addButton());
+    return item;
+}
+    function addButton() {
+    const button = document.createElement('button');
+        button.classList.add('SideBarRightComponentButton')
+    const addBtn = new Image();
+    addBtn.src = plus;
+    button.appendChild(addBtn);
+    return button;
+    }
+    function editButton() {
+    const button = document.createElement('button');
+        button.classList.add('SideBarRightComponentButton');
+    const editImage = new Image();
+    editImage.src = pencil;
+    button.appendChild(editImage);
+    return button;
+    }
+function leftSidebarComponent(title) {
+    const item = document.createElement('div');
+        item.classList.add('SideBarItemLeftComponent');
 
     item.appendChild(header(title));
     return item;
 };
-function headerItemListCollapse(title) {
+function leftSidebarComponentListCollapse(title) {
     const item = document.createElement('div');
-        item.classList.add('SideBarHeaderItem');
+        item.classList.add('SideBarItemLeftComponent');
     item.appendChild(header(title));
     item.appendChild(listCollapse());
     return item
 };
-function header(title) {
+    function header(title) {
     const sideHeader = document.createElement('h3')
     sideHeader.classList.add('sideBarHeader');
     sideHeader.textContent = title;
     return sideHeader;
-};
-function listCollapse() {
+    };
+    function listCollapse() {
     const button = document.createElement('button');
-        button.classList.add('sideBarButton');
+        button.classList.add('listCollapseButton');
     const collapseIcon = new Image();
     collapseIcon.src = chevron;
     button.appendChild(collapseIcon);
     return button;
-};
+    };
+
 
 
 export { sidebarDom }
