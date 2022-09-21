84fea4116876fbb04c5859282838bf6a13dfbfd6
Taylor Barinka <barinkataylor@gmail.com>
2022-09-21 14:07:23 -0500

    restarting project on new branch

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 
 const p = document.createElement('p');
-p.textContent = "fuck sample"
+p.textContent = "fuck sample example"
 const body = document.getElementById('body');
 body.appendChild(p);
